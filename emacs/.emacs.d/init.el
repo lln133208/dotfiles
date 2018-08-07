6b57fc9d6c6527ea5b46582ad97134d62e7a9687
Yuan Ren <lln133208@hotmail.com>
2018-08-07 21:01:31 +0800

    add emacs.d to dotfiles first commit for emacs.d

---
+++ emacs/.emacs.d/init.el
@@ -0,0 +1,2 @@
+(add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
+(require 'init-general)
---
+++ emacs/.emacs.d/lisp/init-general.el
@@ -0,0 +1,39 @@
+;;=============================================
+;; Global configuration
+;;=============================================
+
+
+;; frame title
+(setq frame-title-format "Editor for God @%b")
+
+;; line number in left
+(global-linum-mode t)
+
+;; display line number and column number in mode zone
+(setq column-number-mode t)
+(setq line-number-mode t)
+
+;; disable startup message
+(setq inhibit-startup-message t)
+(setq qnus-inhibit-startup-message t)
+
+;; display current time
+(display-time)
+(setq display-time-24hr-format t)
+(setq display-time-day-and-date t)
+(setq displat-time-interval 10)
+
+;; disable menu bar
+;; nil doesn't work, must be 0
+(menu-bar-mode 0)
+(tool-bar-mode 0)
+(scroll-bar-mode 0)
+
+;; buffer size of kill ring
+(setq kill-ring-max 200)
+
+;; disable auto-backup
+(setq make-backup-files nil) ; stop creating backup~ files
+(setq auto-save-default nil) ; stop creating #autosave# files
+
+(provide 'init-general)
