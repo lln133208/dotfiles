1c7254db076400cd007c51fbb06a5f62648962d9
Yuan Ren <yuan.ren@citrix.com>
2018-10-26 17:19:19 +0800

    common configuration

--- emacs/.emacs.d/lisp/init-general.el
+++ emacs/.emacs.d/lisp/init-general.el
@@ -6,24 +6,27 @@
 ;; frame title
 (setq frame-title-format "Editor for God @%b")
 
+;; make emacs frame fullscreen on start-up
+(add-to-list 'default-frame-alist '(fullscreen . maximized))
+
 ;; enable highlighting current line
-(global-hl-line-mode t)
+;;(global-hl-line-mode 1)
 
 ;; line number in left
-(global-linum-mode t)
+(global-linum-mode 1)
 
 ;; display line number and column number in mode zone
-(setq column-number-mode t)
-(setq line-number-mode t)
+(setq column-number-mode 1)
+(setq line-number-mode 1)
 
 ;; disable startup message
-(setq inhibit-startup-message t)
-(setq qnus-inhibit-startup-message t)
+(setq inhibit-startup-message 1)
+(setq qnus-inhibit-startup-message 1)
 
 ;; display current time
 (display-time)
-(setq display-time-24hr-format t)
-(setq display-time-day-and-date t)
+(setq display-time-24hr-format 1)
+(setq display-time-day-and-date 1)
 (setq displat-time-interval 10)
 
 ;; disable menu bar
@@ -38,5 +41,8 @@
 ;; disable auto-backup
 (setq make-backup-files nil) ; stop creating backup~ files
 (setq auto-save-default nil) ; stop creating #autosave# files
+
+;; replace yes/no with y/n
+(fset 'yes-or-no-p 'y-or-n-p)
 
 (provide 'init-general)
